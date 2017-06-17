755ab0f76394841005c4844b0740aeb8ca9ec168
Mert Simsek <simsek97@gmail.com>
2017-06-17 15:58:48 -0400

    Update simsCheckbox.js

--- simsCheckbox.js
+++ simsCheckbox.js
@@ -10,104 +10,116 @@
  
 (function($) {
 
-  // vars
-  var _mobile = /ipad|iphone|ipod|android|blackberry|windows phone|opera mini|silk/i.test(navigator.userAgent);
+    //vars
+    var _mobile = /ipad|iphone|ipod|android|blackberry|windows phone|opera mini|silk/i.test(navigator.userAgent);
 
-  // Plugin init
-  $.fn.simsCheckbox = function(options) {
+    //plugin init
+    $.fn.simsCheckbox = function(options) {
     
-    var selectorElt = this;
+        var selectorElt = this;
     
-    //settings
-    var settings = $.extend({
+        //settings
+        var settings = $.extend({
     
-        //selector height
-        height: 'auto',
+            //selector height
+            height: 'auto',
+            
+            //element
+            element: "li",
         
-        //element
-        element: "li",
+            //checkbox class
+            checkboxClass: "btn btn-block btn-social",
+            
+            //title icon
+            titleIcon: "square-o",
     
-        //checkbox class
-        checkboxClass: "btn btn-block btn-social",
+            //unchecked class
+            uncheckedClass: "btn-default",
         
-        //title icon
-        titleIcon: "square-o",
-
-        //unchecked class
-        uncheckedClass: "btn-default",
+            //checked class
+            checkedClass: "btn-warning",
+            
+            //select/unselect all button
+            selectAllBtn: false,
     
-        //checked class
-        checkedClass: "btn-warning",
+            //select/unselect text
+            selectAllText: 'Select/Unselect All',
+    
+        }, options);
+
+        return selectorElt.each(function(){
+      
+            //set some css for the selector
+            selectorElt.css({'margin': '0', 'padding': '0'});
+            
+            //set the height of the selector first
+            if(settings.height == 'auto') selectorElt.css('height', 'auto');
+            else selectorElt.css({'height': settings.height, 'overflow': 'auto'});
         
-        //select/unselect all button
-        selectAllBtn: false,
-
-        //select/unselect text
-        selectAllText: 'Select/Unselect All',
-
-    }, options);
-
-    //set some css for the selector
-    selectorElt.css({'margin': '0', 'padding': '0'});
+            //add an identifier class to the elements
+            selectorElt.find(settings.element).addClass('sims-selectable');
+            
+            //get elements and handle
+            selectorElt.find(settings.element).each(function(i) {
+              
+                _elt=$(this), _title=_elt.html();
+                
+                //add checkbox class
+                _elt.addClass(settings.checkboxClass);
+                
+                //add checked or unchecked class
+                if(_elt.hasClass('checked')) _elt.addClass(settings.checkedClass).html('<i class="fa fa-fw fa-check-' + settings.titleIcon + '"></i> ' + _title);
+                else _elt.addClass(settings.uncheckedClass).html('<i class="fa fa-fw fa-' + settings.titleIcon + '"></i> ' + _title);
+                
+                //set click event if it is not disabled
+                _elt.off('click').on('click', function (e) {
     
-    //set the height of the selector first
-    if(settings.height == 'auto') selectorElt.css('height', 'auto');
-    else selectorElt.css({'height': settings.height, 'overflow': 'auto'});
-
-    //add an identifier class to the elements
-    selectorElt.find(settings.element).addClass('sims-selectable');
-    
-    //get elements and handle
-    selectorElt.find(settings.element).each(function(i){
-      _elt=$(this), _title=_elt.html();
-      
-      //add checkbox class
-      _elt.addClass(settings.checkboxClass);
-      
-      //add checked or unchecked class
-      if(_elt.hasClass('checked')) _elt.addClass(settings.checkedClass).html('<i class="fa fa-fw fa-check-' + settings.titleIcon + '"></i> ' + _title);
-      else _elt.addClass(settings.uncheckedClass).html('<i class="fa fa-fw fa-' + settings.titleIcon + '"></i> ' + _title);
-      
-      //set click event if it is not disabled
-      _elt.off('click').on('click', function (e) {
-        e.preventDefault();
+                    e.preventDefault();
+                    
+                    //if the element is disabled then do nothing
+                    if($(this).hasClass('disabled')) return false;
+                    
+                    //toggle the item
+                    $(this).toggleClass(settings.uncheckedClass).toggleClass(settings.checkedClass).find('i').toggleClass('fa-' + settings.titleIcon).toggleClass('fa-check-' + settings.titleIcon);
+                
+                }); //end _elt click
+              
+            }); //end selectorElt each
+            
+            //set checkAll button
+            if(settings.selectAllBtn)
+            {
+              
+                //test all checked or not
+                var allChecked = (selectorElt.find(settings.element).length == selectorElt.find(settings.element + '.checked').length) ? true : false;
+                var selectAllBtnElt = $( '<' + settings.element +  ' class="sims-btn-select-all"></' + settings.element + '>' ).addClass(settings.checkboxClass + ' ' + (allChecked ? settings.checkedClass : settings.uncheckedClass)).html('<i class="fa fa-fw fa-' + (allChecked ? 'check-' : '') + settings.titleIcon + '"></i> '  + settings.selectAllText);
+                
+                selectorElt.append( selectAllBtnElt );
+                
+                //set click event for the selectAll button
+                selectorElt.find('.sims-btn-select-all').off('click').on('click', function (e) {
+                e.preventDefault();
+                
+                //toggle the item
+                $(this).toggleClass(settings.uncheckedClass).toggleClass(settings.checkedClass).find('i').toggleClass('fa-' + settings.titleIcon).toggleClass('fa-check-' + settings.titleIcon);
+                
+                //if all items are selected then select-all button is checked
+                //if one of the items is unselected then select-all button is unchecked
+                if( $(this).hasClass("btn-default") )
+                {
+                    selectorElt.find(settings.element + '.sims-selectable:not(.disabled)').addClass(settings.uncheckedClass).removeClass(settings.checkedClass).find('i').addClass('fa-' + settings.titleIcon).removeClass('fa-check-' + settings.titleIcon);
+                }
+                else
+                {
+                    selectorElt.find(settings.element + '.sims-selectable:not(.disabled)').removeClass(settings.uncheckedClass).addClass(settings.checkedClass).find('i').removeClass('fa-' + settings.titleIcon).addClass('fa-check-' + settings.titleIcon);
+                }
+                
+                });
+              
+            } //end if
         
-        //if the element is disabled then do nothing
-        if($(this).hasClass('disabled')) return false;
+        }); //end return
         
-        //toggle the item
-        $(this).toggleClass(settings.uncheckedClass).toggleClass(settings.checkedClass).find('i').toggleClass('fa-' + settings.titleIcon).toggleClass('fa-check-' + settings.titleIcon);
-
-      });
-    });
+    } //end function
     
-    //set checkAll button
-    if(settings.selectAllBtn)
-    {
-      //test all checked or not
-      var allChecked = (selectorElt.find(settings.element).length == selectorElt.find(settings.element + '.checked').length) ? true : false;
-      var selectAllBtnElt = $( '<' + settings.element +  ' class="sims-btn-select-all"></' + settings.element + '>' ).addClass(settings.checkboxClass + ' ' + (allChecked ? settings.checkedClass : settings.uncheckedClass)).html('<i class="fa fa-fw fa-' + (allChecked ? 'check-' : '') + settings.titleIcon + '"></i> '  + settings.selectAllText);
-      
-      selectorElt.append( selectAllBtnElt );
-      
-      //set click event for the selectAll button
-      selectorElt.find('.sims-btn-select-all').off('click').on('click', function (e) {
-        e.preventDefault();
-
-        //toggle the item
-        $(this).toggleClass(settings.uncheckedClass).toggleClass(settings.checkedClass).find('i').toggleClass('fa-' + settings.titleIcon).toggleClass('fa-check-' + settings.titleIcon);
-
-        //if all items are selected then select-all button is checked
-        //if one of the items is unselected then select-all button is unchecked
-        if( $(this).hasClass("btn-default") )
-        {
-          selectorElt.find(settings.element + '.sims-selectable:not(.disabled)').addClass(settings.uncheckedClass).removeClass(settings.checkedClass).find('i').addClass('fa-' + settings.titleIcon).removeClass('fa-check-' + settings.titleIcon);
-        }
-        else
-        {
-          selectorElt.find(settings.element + '.sims-selectable:not(.disabled)').removeClass(settings.uncheckedClass).addClass(settings.checkedClass).find('i').removeClass('fa-' + settings.titleIcon).addClass('fa-check-' + settings.titleIcon);
-        }
-      });
-    }    
-  }
-})(jQuery);
+}(jQuery));
