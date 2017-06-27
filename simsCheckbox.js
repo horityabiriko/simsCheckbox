130a3eec6432a7fdbc079c4f49f9d7d4464bf70d
Mert Simsek <simsek97@gmail.com>
2017-06-27 13:28:48 -0400

    Radio Button style option added

--- simsCheckbox.js
+++ simsCheckbox.js
@@ -22,6 +22,10 @@
         //settings
         var settings = $.extend({
     
+            //style
+            //set treat as checkbox or radio
+            btnStyle: 'checkbox',
+            
             //selector height
             height: 'auto',
             
@@ -86,6 +90,12 @@
                     //if the element is disabled then do nothing
                     if($(this).hasClass('disabled')) return false;
                     
+                    //if the style is radio kind then unselect all first
+                    if(settings.btnStyle == 'radio')
+                    {
+                        selectorElt.find(settings.element).addClass(settings.uncheckedClass).removeClass(settings.checkedClass).find('i').addClass('fa-' + settings.titleIcon).removeClass('fa-check-' + settings.titleIcon);
+                    }
+                    
                     //toggle the item
                     $(this).toggleClass(settings.uncheckedClass).toggleClass(settings.checkedClass).find('i').toggleClass('fa-' + settings.titleIcon).toggleClass('fa-check-' + settings.titleIcon);
 
@@ -102,12 +112,13 @@
               
                 //test all checked or not
                 var allChecked = (selectorElt.find(settings.element).length == selectorElt.find(settings.element + '.checked').length) ? true : false;
-                var selectAllBtnElt = $( '<' + settings.element +  ' class="sims-btn-select-all"></' + settings.element + '>' ).addClass(checkboxClass + ' ' + (allChecked ? settings.checkedClass : settings.uncheckedClass)).html('<i class="fa fa-fw fa-' + (allChecked ? 'check-' : '') + settings.titleIcon + '"></i> '  + settings.selectAllText);
+                var selectAllBtnContainer = $( '<ul></ul>' ).css({'margin': '5px 0 0 0', 'padding': '0'});
+                var selectAllBtnElt = $( '<' + settings.element +  ' class="sims-btn-select-all"></' + settings.element + '>' ).css('border', '1px dashed').addClass(checkboxClass + ' ' + (allChecked ? settings.checkedClass : settings.uncheckedClass)).html('<i class="fa fa-fw fa-' + (allChecked ? 'check-' : '') + settings.titleIcon + '"></i> '  + settings.selectAllText);
                 
-                selectorElt.append( selectAllBtnElt );
+                selectorElt.after( selectAllBtnContainer.append(selectAllBtnElt) );
                 
                 //set click event for the selectAll button
-                selectorElt.find('.sims-btn-select-all').off('click').on('click', function (e) {
+                selectAllBtnContainer.find('.sims-btn-select-all').off('click').on('click', function (e) {
                     
                     //prevent default events
                     e.preventDefault();
@@ -121,10 +132,10 @@
                     //if all items are selected then select-all button is checked
                     //if one of the items is unselected then select-all button is unchecked
                     selectorElt.find(settings.element + '.sims-selectable:not(.disabled)').each(function(){
-                    
+                        
                         //fix classes of the items
-                        if( thisButton.hasClass("btn-default") ) $(this).addClass(settings.uncheckedClass).removeClass(settings.checkedClass).find('i').addClass('fa-' + settings.titleIcon).removeClass('fa-check-' + settings.titleIcon);
-                        else $(this).removeClass(settings.uncheckedClass).addClass(settings.checkedClass).find('i').removeClass('fa-' + settings.titleIcon).addClass('fa-check-' + settings.titleIcon);
+                        if( thisButton.hasClass("btn-default") ) $(this).removeClass(settings.uncheckedClass).addClass(settings.checkedClass).find('i').removeClass('fa-' + settings.titleIcon).addClass('fa-check-' + settings.titleIcon);
+                        else $(this).addClass(settings.uncheckedClass).removeClass(settings.checkedClass).find('i').addClass('fa-' + settings.titleIcon).removeClass('fa-check-' + settings.titleIcon);
                         
                         //trigger click event for the items
                         $(this).trigger("click");
